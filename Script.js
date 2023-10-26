f610ae6b8060028172202444c559e67a02e58aac
adithyankdev <148231878+adithyankdev@users.noreply.github.com>
2023-10-26 15:57:11 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+function setnewimg(){
+    document.getElementById("bgimg").src="IMAGES/Ice Cream Advertising Bundle Vol_3.jpg";
+
+}
+
+function setoldimg(){
+    document.getElementById("bgimg").src="IMAGES/backimg.png";
+
+}
